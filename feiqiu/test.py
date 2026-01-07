f620e02c2f2b0fc263838941412ebc9763dc54f1
shikun.sh <shikun.sh@cheilpengtai.com>
2026-01-07 16:45:56 +0800

    移除

--- feiqiu/test.py
+++ feiqiu/test.py
@@ -2,7 +2,7 @@ from openai import OpenAI
 
 client = OpenAI(
   base_url = "https://integrate.api.nvidia.com/v1",
-  api_key = "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"
+  api_key = ""
 )
 #   model="minimaxai/minimax-m2",
 completion = client.chat.completions.create(
